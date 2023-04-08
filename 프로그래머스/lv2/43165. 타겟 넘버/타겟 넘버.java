cea2d936872b33f2b174a9e8150cd29873b47091
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-08 15:17:34 +0900

    [level 2] Title: 타겟 넘버, Time: 0.39 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,30 @@
-import java.util.*;
-
 class Solution {
     int answer = 0;
     
-    public void dfs(int[] numbers, int target, int depth,int result){
-        if(depth==numbers.length){
-            if(target==result){
+    public void dfs(int[] numbers, int start, int pick, int total, int target){
+        
+        if(pick==numbers.length){
+            if(total==target) {
                 answer++;
             }
             return;
         }
         
-        
-        dfs(numbers, target, depth+1, result+numbers[depth]);
-        dfs(numbers, target, depth+1, result-numbers[depth]);
-        
+        dfs(numbers,start+1, pick+1, total+numbers[start], target);
+        dfs(numbers,start+1, pick+1, total-numbers[start], target);
         
     }
     
     public int solution(int[] numbers, int target) {
         
         
-        dfs(numbers,target,0,0);
+        //1. +,- 개수 = numbers.length
+        //2. dfs로 구현
+        // - 종료 조건 : pick==numbers.length
+        //      - target과 같을 때 cnt++
+        // - numbers[], start, pick, total
+        
+        dfs(numbers,0,0,0,target);
         
         return answer;
     }
