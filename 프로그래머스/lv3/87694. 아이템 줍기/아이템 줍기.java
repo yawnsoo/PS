a7e3a2e7a49f50b1f5729047b510f8bee1e07649
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-09 23:59:07 +0900

    [level 3] Title: 아이템 줍기, Time: 1.08 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+class Solution {
+    
+    /*
+    1. 전체 도형의 바깥 부분 그리고
+    2. 동서남북으로 최단 거리 -> bfs, queue 이용
+    */
+    int minX = 100, minY = 100, maxX = 0, maxY = 0;
+    int[][] map;
+    boolean[][] visited;
+    int answer = 0;
+    
+    public void outline(int[][] rectangle){
+        
+        for(int i = 0; i<rectangle.length; i++){
+            minX = Math.min(minX,rectangle[i][0]);
+            minY = Math.min(minY,rectangle[i][1]);
+            maxX = Math.max(maxX,rectangle[i][2]);
+            maxY = Math.max(maxY,rectangle[i][3]);
+        }
+        
+        map = new int[maxY*2+2][maxX*2+2];
+        visited = new boolean[maxY*2+2][maxX*2+2];
+        
+        for(int r = 0 ; r<rectangle.length; r++){
+            for(int i = rectangle[r][1]*2; i<=rectangle[r][3]*2; i++){
+                for(int j = rectangle[r][0]*2; j<=rectangle[r][2]*2;j++){
+                        map[i][j]++;
+                }
+            }
+        }
+        
+        for(int i = 0; i<map.length; i++){
+            for(int j = 0; j<map[i].length;j++){
+                try{
+                    if((map[i-1][j]==0||map[i+1][j]==0||map[i][j-1]==0||map[i][j+1]==0)&&map[i][j]==1){
+                        map[i][j]=-1;
+                    }
+                } catch(Exception e) {continue;}
+            }
+        }
+        
+    }
+    
+    public void bfs(int cax, int cay, int ix, int iy) {
+        Queue<int[]> q = new LinkedList<>();
+        
+        int[] dy = {0,1,0,-1};
+        int[] dx = {1,0,-1,0};
+        
+        q.add(new int[]{cay*2,cax*2,1});
+        visited[cay*2][cax*2] = true;
+        
+        while(!q.isEmpty()){
+            int[] p = q.poll();
+            
+            int cy = p[0];
+            int cx = p[1];
+            int depth = p[2];
+            
+            if(cy==iy*2&&cx==ix*2){
+                answer = depth/2;
+                return;
+            }
+            
+            for(int i = 0; i<4; i++){
+                int ny = cy + dy[i];
+                int nx = cx + dx[i];
+                
+                if(ny<0||nx<0||ny>maxY*2||nx>maxX*2) continue;
+                
+                if(map[cy][cx]==-1&&map[ny][nx]==-1&&!visited[ny][nx]){
+                    visited[ny][nx] = true;
+                    q.add(new int[]{ny,nx, depth+1});
+                }
+                
+                if(map[cy][cx]==-1&&map[ny][nx]==2&&!visited[ny][nx]){
+                    visited[ny][nx] = true;
+                    q.add(new int[]{ny,nx, depth+1});
+                }
+                
+                if(map[cy][cx]==2&&map[ny][nx]==-1&&!visited[ny][nx]){
+                    visited[ny][nx] = true;
+                    q.add(new int[]{ny,nx, depth+1});
+                }
+                
+            }
+            
+        }
+    }
+    
+    
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        
+        //바깥 부분 그리기
+        outline(rectangle);
+        
+        
+        //bfs 최단거리
+        bfs(characterX, characterY, itemX, itemY);
+        
+        
+        return answer;
+    }
+}
