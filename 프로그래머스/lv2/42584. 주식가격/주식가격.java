0288ccb3a1c1f43c4ca9fb581445c31c89a7c59e
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-12 03:44:05 +0900

    [level 2] Title: 주식가격, Time: 20.37 ms, Memory: 66.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -2,30 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
+        
         int[] answer = new int[prices.length];
-        
-        //(idx) 0 1 2 3 4 5 6 7 8 9
-        //(pri) 1 2 3 4 2 3 2 1 4 3
-        //(ans) 9 6 2 1 3 1 1 2 1 0
-        
         Stack<Integer> s = new Stack<>();
+        
         for(int i = 0 ; i<prices.length-1; i++){
             //pri=1 이면 확정
             if(prices[i]==1){
                 answer[i] = prices.length-i-1;
+                
             //값 증가/유지 면 stack에 넣고
             } else if(prices[i]<=prices[i+1]){
                 s.push(i);
-                // System.out.println(s);
+
             //값 줄어들면 answer[idx]에 값 지정
             } else {
                 s.push(i);
-                // System.out.println(s);
                 while(prices[s.peek()]>prices[i+1]){
-                    //i = 4, idx = 3, pri = 4
                     int idx = s.pop();
                     int pri = prices[idx];
-                    // System.out.printf("idx : %d, pri : %d\n",idx,pri);
                     answer[idx] = i+1 - idx;
                     
                     if(s.isEmpty()) break;
@@ -33,17 +28,11 @@ class Solution {
             }
         }
         
-        
-        // System.out.println("end loop");
-        // System.out.println(s);
-        
-        
-            while(!s.isEmpty()){
-                int temp = s.pop();
-                answer[temp] = prices.length-temp-1;
-            }
-        
-        
+        //stack에 값이 마지막까지 남아 있는 경우
+        while(!s.isEmpty()){
+            int temp = s.pop();
+            answer[temp] = prices.length-temp-1;
+        }
         
         return answer;
     }
