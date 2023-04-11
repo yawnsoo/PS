b3126f1803172802f2f7e49c1e8eb58c57816eb4
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-11 20:06:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.16 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,57 +1,67 @@
 import java.util.*;
 
 class Solution {
-    class Truck {
-        int weight;
-        int move;
+    int answer = 0;
+    int cnt = 0;
+    int inBridge = 0;
 
-        public Truck(int weight) {
-            this.weight = weight;
-            this.move = 1;
-        }
-
-        public void moving() {
-            move++;
-        }
+    public void addTruckToBridge(Queue<Integer> trucks, Queue<Integer> bridge, Queue<Integer> timeIn){
+        int truckIn = trucks.poll();
+        //  - 다리q에 무게 추가
+        bridge.offer(truckIn);
+        //  - 총 무게에 추가
+        inBridge += truckIn;
+        //  - 시각q에 들어간 시간 추가
+        timeIn.offer(answer);
     }
 
-    public int solution(int bridgeLength, int weight, int[] truckWeights) {
-        Queue<Truck> waitQ = new LinkedList<>();
-        Queue<Truck> moveQ = new LinkedList<>();
+    public void passedTruckFromBridge(Queue<Integer> bridge, Queue<Integer> timeIn){
+        //  - 시간q.pop()
+        timeIn.poll();
+        //  - 총 무게에 빼기
+        inBridge -= bridge.poll();
+        //  - cnt++
+        cnt++;
+    }
 
-        for (int t : truckWeights) {
-            waitQ.offer(new Truck(t));
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+
+
+        //queue 3개로 구현 : 다리q, 시각q, 트럭무게q
+        Queue<Integer> bridge = new LinkedList<>();
+        Queue<Integer> timeIn = new LinkedList<>();
+        Queue<Integer> trucks = new LinkedList<>();
+
+        for(int i : truck_weights){
+            trucks.offer(i);
         }
 
-        int answer = 0;
-        int curWeight = 0;
+        //첫 트럭은 무조건 건넘, 초기화
+        answer++;
+        addTruckToBridge(trucks, bridge, timeIn);
 
-        while (!waitQ.isEmpty() || !moveQ.isEmpty()) {
+
+        //3. cnt == 트럭의 개수 이면 종료
+        //  - 빼고
+        //  - 더하고
+        while(cnt<truck_weights.length){
             answer++;
 
-            if (moveQ.isEmpty()) {
-                Truck t = waitQ.poll();
-                curWeight += t.weight;
-                moveQ.offer(t);
-                continue;
+            //1. 시각q.pop()+다리 길이 의 시간에 도달 할 경우
+            if(timeIn.peek()+bridge_length == answer){
+                passedTruckFromBridge(bridge, timeIn);
             }
 
-            for (Truck t : moveQ) {
-                t.moving();
-            }
-
-            if (moveQ.peek().move > bridgeLength) {
-                Truck t = moveQ.poll();
-                curWeight -= t.weight;
-            }
-
-            if (!waitQ.isEmpty() && curWeight + waitQ.peek().weight <= weight) {
-                Truck t = waitQ.poll();
-                curWeight += t.weight;
-                moveQ.offer(t);
+            //2. 다리(queue)에 올라가 있는 무게 총값 + 들어갈 트럭 <= 다리가 견딜 수 있는 무게
+            if(!trucks.isEmpty()){ //트럭이 남아 있을 경우에
+                if(inBridge+trucks.peek()<=weight){
+                    addTruckToBridge(trucks, bridge, timeIn);
+                }
             }
         }
 
+        //4. 시각 return;
         return answer;
     }
 }
