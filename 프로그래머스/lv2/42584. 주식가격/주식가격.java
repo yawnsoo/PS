b9f913bf7df99e332b920f5f78a828a3da5c863a
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-12 04:01:43 +0900

    [level 2] Title: 주식가격, Time: 25.67 ms, Memory: 67.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,15 +1,35 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int[] prices) {
-        int len = prices.length;
-        int[] answer = new int[len];
-        int i, j;
-        for (i = 0; i < len; i++) {
-            for (j = i + 1; j < len; j++) {
-                answer[i]++;
-                if (prices[i] > prices[j])
-                    break;
+        public int[] solution(int[] prices) {
+
+            int[] answer = new int[prices.length];
+            Stack<Integer> s = new Stack<>();
+
+            for(int i = 0 ; i<prices.length-1; i++){
+                //pri=1 이면 확정
+                if(prices[i]==1){
+                    answer[i] = prices.length-i-1;
+
+                //stack에 idx값 넣고 비교
+                } else {
+                    s.push(i);
+                    while(prices[s.peek()]>prices[i+1]){
+                        int idx = s.pop();
+                        int pri = prices[idx];
+                        answer[idx] = i+1 - idx;
+
+                        if(s.isEmpty()) break;
+                    }
+                }
             }
+
+            //stack에 값이 마지막까지 남아 있는 경우
+            while(!s.isEmpty()){
+                int temp = s.pop();
+                answer[temp] = prices.length-temp-1;
+            }
+
+            return answer;
         }
-        return answer;
     }
-}
