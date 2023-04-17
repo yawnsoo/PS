890b55eb45ece593fad0966b424d286a97327cac
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-04-18 08:30:36 +0900

    Create palindromeIndex2.java

---
+++ HackerRank/1 Week Preparation Kit/day3/palindromeIndex2.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+//투 포인터로 풀음
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+    // Write your code here
+    int start = 0;
+    int end = s.length()-1;
+    
+    while(start<end){
+        if(s.charAt(start)!=s.charAt(end)){
+            break;
+        }
+        
+        start++;
+        end--;
+    }
+    
+    if(start>=end) return -1;
+    
+    int l = start;
+    int r = end;
+    start++;
+    
+    while(start<end){
+        if(s.charAt(start)!=s.charAt(end)){
+            return r;
+        }
+        
+        start++;
+        end--;
+    }
+    
+    if(start>=end) return l;
+    
+    return -1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
