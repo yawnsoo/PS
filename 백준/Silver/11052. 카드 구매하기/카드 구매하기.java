968fbe56286ec792dd9e56d6d95ee7579cdbb144
정연수 <54070738+yawnsoo@users.noreply.github.com>
2023-05-05 18:35:10 +0900

    [Silver I] Title: 카드 구매하기, Time: 296 ms, Memory: 21208 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] cardPacks;
+    static int[] dp;
+
+
+    public static void solution() {
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = cardPacks[i];
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + cardPacks[j]);
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        cardPacks = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            cardPacks[i] = sc.nextInt();
+        }
+        dp = new int[n+1];
+
+        solution();
+
+        System.out.println(dp[n]);
+    }
+}
